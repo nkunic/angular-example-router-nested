a41dfc2b032fdcef92e9cc841dba1e15871dc08e
nkunic <45877400+nkunic@users.noreply.github.com>
2023-05-26 19:35:45 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { CompanyComponent } from "./about/company/company.component";
+import { EmployeesComponent } from "./about/employees/employees.component";
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 const myRoutes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
     path: '',
     redirectTo: '/home',
     pathMatch: 'full',
   },
-
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
   {
     path: 'about',
-    component: AboutComponent,
+    children: [
+      {
+        path: "",
+        component: AboutComponent,
+      },
+      {
+        path: "employees",
+        component: EmployeesComponent,
+      },
+      {
+        path: "company",
+        component: CompanyComponent,
+      },
+    ],
   },
   {
     path: 'contact',
