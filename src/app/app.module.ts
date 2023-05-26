89327fdaa94e760b1c1c0e494c222fd558241c64
nkunic <45877400+nkunic@users.noreply.github.com>
2023-05-26 17:03:31 +0200

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
 import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent }
- ];
-
 @NgModule({
   imports: [
-    BrowserModule, 
+    BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule,
   ],
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent
-  ],
+  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent],
   exports: [RouterModule],
   bootstrap: [AppComponent],
 })
