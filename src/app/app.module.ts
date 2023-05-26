73f04f93c2afc62522add8e3eaa209b62ad36171
nkunic <45877400+nkunic@users.noreply.github.com>
2023-05-26 16:49:59 +0200

    Router basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+ ];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  exports: [RouterModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
