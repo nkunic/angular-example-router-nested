8b286e371c70e62fe0ecf28d6cb2cb14d5b670e1
nkunic <45877400+nkunic@users.noreply.github.com>
2023-05-28 11:59:04 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { CompanyComponent } from './about/company/company.component';
+import { EmployeesComponent } from './about/employees/employees.component';
 import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    CompanyComponent,
+    EmployeesComponent,
+    ContactComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
